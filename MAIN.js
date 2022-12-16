e82f7ea6236ff25bba0bc358cd93213451bdbcbe
Soham-Here <104490428+Soham-Here@users.noreply.github.com>
2022-12-16 11:06:34 +0530

    Add files via upload

---
+++ MAIN.js
@@ -0,0 +1,20 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(400,300);
+canvas.position(450,200);
+}
+
+function draw(){
+
+}
+
+function mustache_filter_pic_png(){
+    save("mustache_pic.png");
+}
+
+function mustache_filter_pic_jpg(){
+    save("mustache_pic.jpg");
+}
